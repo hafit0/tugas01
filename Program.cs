98305ac35ddc1516bfe94cb5b7459f94050a19b4
hafit0 <61793573+hafit0@users.noreply.github.com>
2020-03-13 17:28:16 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,47 +16,33 @@ namespace pertemuan_1
             Console.WriteLine("1. Pertambahan \n2. Pengurangan \n3. Perkalian \n4. Pembagian ");
             Console.WriteLine("\nmasukkan pilihan :");
             input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Masukkan angka pertama :");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Masukkan angka kedua :");
+            b = Convert.ToInt32(Console.ReadLine());
+            
             switch (input)
             {
                 case 1:
-                    Console.WriteLine("Masukkan angka pertama :");
-                    a = Convert.ToInt32(Console.ReadLine());
-
-                    Console.WriteLine("Masukkan angka kedua :");
-                    b = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("hasil penambahan  {0} + {1} = {2} ", a, b, penambahan(a, b));
                     Console.WriteLine("\n tekan enter untuk keluar");
                     Console.ReadKey();
                     break;
 
                 case 2:
-                    Console.WriteLine("Masukkan angka pertama : ");
-                    a = Convert.ToInt32(Console.ReadLine());
-
-                    Console.WriteLine("Masukkan angka kedua : ");
-                    b = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("hasil pengurangan  {0} - {1} = {2} ", a, b, pengurangan(a, b));
                     Console.WriteLine("\n tekan enter untuk keluar");
                     Console.ReadKey();
                     break;
 
                 case 3:
-                    Console.WriteLine("Masukkan angka pertama : ");
-                    a = Convert.ToInt32(Console.ReadLine());
-
-                    Console.WriteLine("Masukkan angka kedua : ");
-                    b = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("hasil perkalian  {0} * {1} = {2} ", a, b, perkalian(a, b));
                     Console.WriteLine("\n tekan enter untuk keluar");
                     Console.ReadKey();
                     break;
 
                 case 4:
-                    Console.WriteLine("Masukkan angka pertama : ");
-                    a = Convert.ToInt32(Console.ReadLine());
-
-                    Console.WriteLine("Masukkan angka kedua : ");
-                    b = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("hasil pembagian  {0} / {1} = {2} ", a, b, pembagian(a, b));
                     Console.WriteLine("\n tekan enter untuk keluar");
                     Console.ReadKey();
